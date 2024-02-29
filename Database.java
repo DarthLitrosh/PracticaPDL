efe115c80b3107598f547c86b4f1aba77573dfdd
Carlos Enrique <carlosnuipe@gmail.com>
2024-02-29 21:03:19 +0100

    "basedatos"

---
+++ Database.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/tu_base_de_datos";
+    private static final String DATABASE_USER = "tu_usuario";
+    private static final String DATABASE_PASSWORD = "tu_contraseña";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
