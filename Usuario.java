72c0baf5630b96def0cdd2d076ad41773a3627c9
oiJosukee <75758128+oiJosukee@users.noreply.github.com>
2024-04-02 20:41:18 +0200

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,12 @@
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Usuario {
+    private String id;
+    private String nombreUsuario;
+    private String fechaNacimiento;
+    private String email;
+
+    // Constructor, getters y setters
+  
+}
